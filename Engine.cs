0e6d22d40a12aff2bdb3a10aca8b2ecf0e9b1682
DarkLotus <kklolk@gmail.com>
2019-04-20 15:41:16 +1000

    Fix build

--- Engine.cs
+++ Engine.cs
@@ -31,7 +31,7 @@ namespace Assistant
 
         public static bool UsePostKRPackets => ClientVersion >= ClientVersions.CV_6017;
         private static string _rootPath = null;
-        public static string RootPath => _rootPath ?? ( _rootPath = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location ) );
+        public static string RootPath => _rootPath ?? ( _rootPath = Path.GetDirectoryName( Assembly.GetAssembly( typeof( Engine ) ).Location ) );
         public static string UOFilePath { get; internal set; }
         public static bool IsInstalled { get; internal set; }
         public static CEasyUOMainForm m_MainForm;
@@ -39,6 +39,23 @@ namespace Assistant
         public static unsafe void Install( PluginHeader* header )
         {
             Console.WriteLine( "Install Invoked CEasyUO" );
+            AppDomain.CurrentDomain.AssemblyResolve += ( sender, e ) =>
+            {
+                string[] fields = e.Name.Split( ',' );
+                string name = fields[0];
+                string culture = fields[2];
+
+                if ( name.EndsWith( ".resources" ) && !culture.EndsWith( "neutral" ) )
+                {
+                    return null;
+                }
+                AssemblyName askedassembly = new AssemblyName( e.Name );
+
+                bool isdll = File.Exists( Path.Combine( RootPath, askedassembly.Name + ".dll" ) );
+
+                return Assembly.LoadFile( Path.Combine( RootPath, askedassembly.Name + ( isdll ? ".dll" : ".exe" ) ) );
+
+            };
             try
             {
                 ClientVersion = (ClientVersions)header->ClientVersion;
